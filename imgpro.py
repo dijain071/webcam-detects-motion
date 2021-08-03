4041b3839f8891cf604f54a73ed1c0698b653166
dijain071 <75722516+dijain071@users.noreply.github.com>
2021-08-03 11:04:41 +0530

    Update imgpro.py

--- imgpro.py
+++ imgpro.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread("Lighthouse.jpg",1)
+img = cv2.imread("me.jpg",1)
 
 print(type(img))
 print(img)
@@ -10,7 +10,7 @@ print(img.ndim)
 # 1000 = width
 # 500 = length
 resize_image = cv2.resize(img,(100,100))
-cv2.imshow("Lighthouse",resize_image)
-cv2.imwrite("LH_RESIZED.jpg",resize_image)
+cv2.imshow("me",resize_image)
+cv2.imwrite("RESIZED.jpg",resize_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
